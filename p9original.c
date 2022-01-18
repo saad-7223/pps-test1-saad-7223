3db52b9517e48878b758e3ab1537d4d6621c7814
saad7223 <88393775+saad-7223@users.noreply.github.com>
2022-01-18 15:11:46 +0000

    p9original.c,p9final.c,p9debuglog.txt,saad

--- p9original.c
+++ p9original.c
@@ -1,1 +1,52 @@
- 
+#include<stdio.h>
+
+void input_two_string(char *a,char *b){
+    printf("Enter a string : ");
+    scanf("%s",a);
+    printf("Enter a string : ");
+    scanf("%s",b);  
+}
+int cmp(char *a,char *b){
+  int result,i=0;
+  while(a[i]!='\0' && b[i]!='\0'){
+      if(a[i]>b[i]){
+        result = -1;
+        break;
+      }
+      else if(b[i]>a[i]){
+        result=1;
+        break;
+      }
+      else{
+        i++; 
+      }
+  }
+  if(a[i]=='\0'){
+    result=-1;
+  }
+  else if(b[i]=='\0'){
+    result=1;
+  }
+  if(a[i]=='\0' && b[i]=='\0'){
+    result=0;
+  }
+  return result;
+}
+void output(char *a,char *b,int ans){
+    if(ans==-1){
+      printf("%s is greater than %s",a,b);
+    }
+    else if(ans==1){
+      printf("%s is greater than %s",b,a);
+    }
+    else{
+      printf("%s is equal to %s",a,b);
+    }
+}
+int main(){
+   int ans;
+   char a[20],b[20];
+   input_two_string(a,b);
+   ans=cmp(a,b);
+   output(a,b,ans);
+}
